79909042b7d3b0453e0f371f08b29e0e9eebef05
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-04-24 04:20:32 +0200

    SSR

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -17,39 +17,26 @@ export default function postId({ post }) {
   );
 }
 
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10"
-  );
-  const posts = await res.json();
+// export async function getStaticPaths() {
+//   // Call an external API endpoint to get posts
+//   const res = await fetch(
+//     "https://jsonplaceholder.typicode.com/posts?_limit=10"
+//   );
+//   const posts = await res.json();
 
-  // Get the paths we want to prerender based on posts
-  // In production environments, prerender all pages
-  // (slower builds, but faster initial page load)
-  const paths = posts.map((post) => ({
-    params: { postId: `${post.id}` }, //must be postId as the filename and route
-  }));
-  return {
-    paths: paths,
-    fallback: false, // can also be true or 'blocking'
-  };
-}
+//   // Get the paths we want to prerender based on posts
+//   // In production environments, prerender all pages
+//   // (slower builds, but faster initial page load)
+//   const paths = posts.map((post) => ({
+//     params: { postId: `${post.id}` }, //must be postId as the filename and route
+//   }));
+//   return {
+//     paths: paths,
+//     fallback: false, // can also be true or 'blocking'
+//   };
+// }
 
-export async function getStaticProps(context) {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.postId}`
-  );
-  const data = await res.json();
-  return {
-    props: {
-      post: data,
-    }, // will be passed to the page component as props
-  };
-}
-
-// export async function getServerSideProps(context) {
-// Everytime makes a request
+// export async function getStaticProps(context) {
 //   const res = await fetch(
 //     `https://jsonplaceholder.typicode.com/posts/${context.params.postId}`
 //   );
@@ -60,3 +47,16 @@ export async function getStaticProps(context) {
 //     }, // will be passed to the page component as props
 //   };
 // }
+
+export async function getServerSideProps(context) {
+  // Everytime makes a request
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.postId}`
+  );
+  const data = await res.json();
+  return {
+    props: {
+      post: data,
+    }, // will be passed to the page component as props
+  };
+}
